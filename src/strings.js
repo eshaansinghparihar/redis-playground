0cc04c27a3fcd7ca72a2c87db7f14a5909d66fc5
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2025-02-10 23:49:23 +0530

    Explored multiple get and set methods

--- src/strings.js
+++ src/strings.js
@@ -1,9 +1,9 @@
 const client = require('./redisClient');
 
 async function init(){
-    // await client.set('user:1','John Doe');
-    // await client.expire('user:1', 10);
-    const result = await client.get('user:1');
+    await client.mset('user:1','John Doe', "user:2", "Bob", "user:3","Alice");
+    await client.expire('user:1', 10);
+    const result = await client.mget('user:1',"user:2","user:3","user:4");
     console.log('Result->',result);
 }
 init();
